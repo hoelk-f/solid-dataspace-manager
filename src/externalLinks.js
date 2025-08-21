67fb58f360188a58b3b08893e2477a09ecb7bba2
Florian Hölken <hoelken.florian@web.de>
2025-08-21 13:19:07 +0200

    refactor: tidy external link handling

--- src/externalLinks.js
+++ src/externalLinks.js
@@ -1,6 +1,22 @@
 export const externalLinks = [
-  { slug: "catalog", title: "Semantic Data Catalog", url: "https://semantic-data-catalog.com" },
-  { slug: "plasma", title: "PLASMA", url: "http://plasma.uni-wuppertal.de/modelings" },
-  { slug: "nodered", title: "Node-RED", url: "https://tmdt-solid-community-server.de/nodered" },
-  { slug: "heat", title: "Urban Heat Monitoring", url: "https://smart-city-urban-heat-monitoring.com" }
+  {
+    slug: "catalog",
+    title: "Semantic Data Catalog",
+    url: "https://semantic-data-catalog.com",
+  },
+  {
+    slug: "plasma",
+    title: "PLASMA",
+    url: "http://plasma.uni-wuppertal.de/modelings",
+  },
+  {
+    slug: "nodered",
+    title: "Node-RED",
+    url: "https://tmdt-solid-community-server.de/nodered",
+  },
+  {
+    slug: "heat",
+    title: "Urban Heat Monitoring",
+    url: "https://smart-city-urban-heat-monitoring.com",
+  },
 ];
